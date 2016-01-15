d9021b48266884b8e76c012e89f5f96d86485e0c
André Diego Piske <andre@jobscore.com>
2016-01-15 13:10:01 -0200

    json instead of inspect

--- app/controllers/peterpans_controller.rb
+++ app/controllers/peterpans_controller.rb
@@ -4,7 +4,7 @@ class PeterpansController < ApplicationController
   end
 
   def show
-    hl = HookLog.new(the_params: "Hey, looks those params: #{params.inspect}")
+    hl = HookLog.new(the_params: params.to_json)
     hl.save!
 
     # Rails.logger.info("Hey, looks those params: #{params.inspect}")
